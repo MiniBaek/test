20ad3931963dfe8a08a98525fdc5e8e77b20065a
MiniBaek <133174173+MiniBaek@users.noreply.github.com>
2023-07-16 17:28:56 +0900

    Create TargetDeviceInfo.java

---
+++ 4/TargetDeviceInfo.java
@@ -0,0 +1,18 @@
+package mytest4.info;
+
+import java.util.List;
+import java.util.Map;
+
+public class TargetDeviceInfo {
+
+	private List<Map<String, String>> targetDevices;
+
+	public List<Map<String, String>> getTargetDeviceList() {
+		return targetDevices;
+	}
+
+	public void setTargetDeviceList(List<Map<String, String>> targetDeviceList) {
+		this.targetDevices = targetDeviceList;
+	}
+	
+}
