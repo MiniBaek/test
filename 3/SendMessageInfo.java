93d14074d7b80209a60ca1ca83dea866b7f22160
MiniBaek <133174173+MiniBaek@users.noreply.github.com>
2023-07-16 17:21:22 +0900

    Create SendMessageInfo.java

---
+++ 3/SendMessageInfo.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import java.util.Map;
+
+public class SendMessageInfo {
+
+	private String Message;
+
+	public String getMessage() {
+		return Message;
+	}
+
+	public void setMessage(String message) {
+		Message = message;
+	}
+}
