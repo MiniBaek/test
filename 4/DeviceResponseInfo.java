c9f4cc85c581a99c982c2401a5d8c2e4147d060b
MiniBaek <133174173+MiniBaek@users.noreply.github.com>
2023-07-16 17:25:29 +0900

    Create DeviceResponseInfo.java

---
+++ 4/DeviceResponseInfo.java
@@ -0,0 +1,19 @@
+package mytest4.info;
+
+public class DeviceResponseInfo {
+
+	private String result;
+
+	public DeviceResponseInfo(String str) {
+		this.result = str;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+}
