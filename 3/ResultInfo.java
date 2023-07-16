77fcc2e78fef05bbfcc2e94d6758db4c67e48619
MiniBaek <133174173+MiniBaek@users.noreply.github.com>
2023-07-16 17:19:32 +0900

    Create ResultInfo.java

---
+++ 3/ResultInfo.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class ResultInfo {
+
+	private String result;
+	public ResultInfo(String str) {
+		this.result = str;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+}
