8418187c8950046f4d22fa23e785328bb93ddb06
MiniBaek <133174173+MiniBaek@users.noreply.github.com>
2023-07-16 17:29:14 +0900

    Create TargetDeviceInfo2.java

---
+++ 4/TargetDeviceInfo2.java
@@ -0,0 +1,40 @@
+package mytest4.info;
+
+public class TargetDeviceInfo2 {
+
+	private String id;
+	private String name;
+	private String type;
+	private String hostName;
+	private Integer port;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getHostName() {
+		return hostName;
+	}
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+	public Integer getPort() {
+		return port;
+	}
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+}
