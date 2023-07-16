67aa288d45a61acb406cf05cadb8797a0897b5c1
MiniBaek <133174173+MiniBaek@users.noreply.github.com>
2023-07-16 17:26:06 +0900

    Create ServerRequestInfo.java

---
+++ 4/ServerRequestInfo.java
@@ -0,0 +1,29 @@
+package mytest4.info;
+
+public class ServerRequestInfo {
+
+	String command;
+	String[] targetDevices;
+	String param;
+	
+	public String getCommand() {
+		return command;
+	}
+	public void setCommand(String command) {
+		this.command = command;
+	}
+	public String[] getTargetDevices() {
+		return targetDevices;
+	}
+	public void setTargetDevices(String[] targetDevices) {
+		this.targetDevices = targetDevices;
+	}
+	public String getParam() {
+		return param;
+	}
+	public void setParam(String param) {
+		this.param = param;
+	}
+	
+	
+}
