397d8cac74ad0ae88cbefa0393fb258ca261d3af
MiniBaek <133174173+MiniBaek@users.noreply.github.com>
2023-07-16 17:28:02 +0900

    Create ServerRequestList.java

---
+++ 4/ServerRequestList.java
@@ -0,0 +1,18 @@
+package mytest4.info;
+
+import java.util.List;
+
+public class ServerRequestList {
+
+	private List<ServerRequestInfo> serverRequestList;
+
+	public List<ServerRequestInfo> getServerRequestList() {
+		return serverRequestList;
+	}
+
+	public void setServerRequestList(List<ServerRequestInfo> serverRequestList) {
+		this.serverRequestList = serverRequestList;
+	}
+	
+	
+}
