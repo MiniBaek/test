5c4692a6d8fdf5202692a1d88b894401019b9d19
MiniBaek <133174173+MiniBaek@users.noreply.github.com>
2023-07-16 17:19:02 +0900

    Create QueueInfo.java

---
+++ 3/QueueInfo.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import java.util.Map;
+
+public class QueueInfo {
+
+	private int QueueSize;
+
+	public int getQueueSize() {
+		return QueueSize;
+	}
+
+	public void setQueueSize(int queueSize) {
+		QueueSize = queueSize;
+	}
+
+	
+	
+}
