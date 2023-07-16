196a3877f063c326a2fdd68f31b738bd524c67fc
MiniBaek <133174173+MiniBaek@users.noreply.github.com>
2023-07-16 17:24:27 +0900

    Create CommandInfo.java

---
+++ 4/CommandInfo.java
@@ -0,0 +1,18 @@
+package mytest4.info;
+
+import java.util.List;
+import java.util.Map;
+
+public class CommandInfo {
+
+	private List<Map<String, String>> commands;
+
+	public List<Map<String, String>> getCommandList() {
+		return commands;
+	}
+
+	public void setCommandList(List<Map<String, String>> commandList) {
+		this.commands = commandList;
+	}
+	
+}
